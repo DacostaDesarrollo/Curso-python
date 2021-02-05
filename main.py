6e5651a457a113cee387479f0d869f36d1a27f76
Daniel Acosta Ceballos <Danny3549@hotmail.com>
2021-02-05 04:49:45 +0000

    comentarios, tipos de datos, concatenación

--- main.py
+++ main.py
@@ -3,4 +3,29 @@
 #x = input()
 #print("Hola "+x)
 
-print(["Daniel",34])
+x = 35
+mensaje = "Hola a todos y todas"
+
+un_integer = 2
+un_float = 2.1
+
+print(un_integer + 3)
+
+print(15**2)
+print(2**10)
+print(9**3)
+
+#raiz cuadrada
+print(4 ** 0.5)
+
+#modulo %
+print(29 % 5)
+print(32 % 3)
+print(44 % 2)
+# concatenación 
+cumple_uno ="Yo tengo "
+edad = 30
+cumple_dos = " años"
+
+mensaje_completo = cumple_uno + str(edad) + cumple_dos
+print(mensaje_completo)
